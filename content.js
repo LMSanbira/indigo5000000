9102e0419176aa7bf6e779a87f28a4adc440ed68
LMSanbira <matan@loadmill.com>
2020-09-01 19:43:16 +0300

    more indigo

--- content.js
+++ content.js
@@ -1,8 +1,79 @@
-chrome.runtime.onMessage.addListener((req) => {
-    setIndigo(req);
+
+chrome.runtime.onMessage.addListener((req, _sender, sendResponse) => {
+    if (req.type === 'borderChange') {
+        setIndigoBorder(req);
+    } else if (req.type === 'more') {
+        moreIndigo();
+    } else if (req.type === 'evenMore') {
+        EvenMoreIndigo();
+    } else if (req.type === 'evenMoreOfTheMore') {
+        EvenMoreOfTheMoreIndigo();
+    } else if (req.type === 'remove') {
+        remove();
+    }
+    
+    sendResponse({moreStage: req.type === 'getStage' ? getMoreStage() : null});
+
+    if(req.isSetStage) {
+        setMoreStage(req.moreStage);
+    }
 });
 
-function setIndigo(indigoData) {
+function setIndigoBorder(indigoData) {
+    const indigoStyle = getIndigoStyle();
+    
+    indigoStyle.innerHTML += `
+    ${indigoData.selector} {
+        border: solid 2px #${indigoData.color} !important;
+    }`;
+}
+
+function moreIndigo() {
+    const indigoStyle = getIndigoStyle();
+    
+    indigoStyle.innerHTML += `
+    *, *::after, *::before {
+        color: #283593 !important;
+    }`;
+}
+
+function EvenMoreIndigo() {
+    const indigoStyle = getIndigoStyle();
+    
+    indigoStyle.innerHTML += `
+    *, *::after, *::before {
+        background-color: #7986cb !important;
+    }`;
+}
+
+function EvenMoreOfTheMoreIndigo() {
+    const indigoStyle = getIndigoStyle();
+
+    indigoStyle.innerHTML += `
+    *, *::after, *::before {
+        animation: pulse-indigo 5s infinite linear;
+    }
+    
+    @keyframes pulse-indigo {
+        0% {
+            box-shadow: 0 0 #c5cae9;
+        }
+        50% {
+            box-shadow: 0 0 69px 69px #1a237e;
+        }
+        100% {
+            box-shadow: 0 0 #c5cae9;
+        }
+    }
+    `;
+}
+
+function remove() {
+    const indigoStyle = getIndigoStyle();
+    indigoStyle.innerHTML = '';
+}
+
+function getIndigoStyle() {
     const elemId = 'indigo5000000';
     let indigoStyle = document.getElementById(elemId);
     if (!indigoStyle) {
@@ -11,8 +82,13 @@ function setIndigo(indigoData) {
         document.head.appendChild(indigoStyle);
     }
 
-    indigoStyle.innerHTML = `
-    ${indigoData.selector} {
-        border: solid 2px #${indigoData.color};
-    }`;
+    return indigoStyle;
+}
+
+function setMoreStage(stage) {
+    window.sessionStorage.setItem('indigo5000000Stage', stage);
+}
+
+function getMoreStage() {
+    return parseInt(window.sessionStorage.getItem('indigo5000000Stage'));
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,71 @@
-document.querySelector(".btn").addEventListener("click", (e) => {
-    chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, getIndigoData());
-    })
+const selectorInput = document.querySelector("#selector");
+const colorInput = document.querySelector("#color");
+const borderBtn = document.querySelector("#borderBtn");
+const moreBtn = document.querySelector("#moreBtn");
+const removeBtn = document.querySelector("#removeBtn");
+
+const moreStages = ['more', 'evenMore', 'evenMoreOfTheMore']
+const moreTexts = ['More Indigo', 'I said MORE!', 'MOOOOOOORE!!!']
+let moreStage = null;
+
+function init() {
+    handleSendMessage('getStage', false, setMoreStage);
+}
+
+borderBtn.addEventListener("click", (_e) => {
+    if(moreStage === null) {
+        setMoreStage({moreStage: 0})
+    }
+    handleSendMessage('borderChange');
+});
+
+moreBtn.addEventListener("click", (_e) => {
+    moreStage++;
+    handleSendMessage(moreStages[moreStage - 1]);
+    setMoreStage({moreStage: moreStage});
+});
+
+removeBtn.addEventListener("click", (_e) => {
+    reset();
+    handleSendMessage('remove');
+});
+
+colorInput.addEventListener("change", (_e) => {
+    document.body.style.background = '#' + colorInput.value;
 });
 
 function getIndigoData() {
-    const selector = document.querySelector("#selector").value;
-    const color = document.querySelector("#color").value;
+    const selector = selectorInput.value;
+    const color = colorInput.value;
 
-    console.log('in');
     return {selector: selector, color: color};
 }
+
+function setMoreStage(status) {
+    if(status.moreStage !== null && status.moreStage !== undefined) {
+        moreStage = status.moreStage;
+        if(status.moreStage < moreStages.length) {
+            moreBtn.innerHTML = moreTexts[moreStage];
+            moreBtn.classList.remove('hide');
+        }
+        else {
+            moreBtn.classList.add('hide');
+            removeBtn.classList.remove('hide');
+        }
+    }
+}
+
+function handleSendMessage(type, isSetStage = true, getResponse = () => { }) {
+    chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+        const baseReq = {type: type, moreStage: moreStage, isSetStage: isSetStage};
+        chrome.tabs.sendMessage(tabs[0].id, Object.assign(baseReq, getIndigoData()), getResponse);
+    });
+}
+
+function reset() {
+    moreStage = null;
+    moreBtn.classList.add('hide');
+    removeBtn.classList.add('hide');
+}
+
+init();
